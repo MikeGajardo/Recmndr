af2cc72eacf9f0b1dad3d6c9c493c6516cc8fdbd
Luffy85 <macias85ale@gmail.com>
2022-08-04 00:25:09 +0000

    actualizado 2

--- src/fireBase.js
+++ src/fireBase.js
@@ -12,5 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 export default app
