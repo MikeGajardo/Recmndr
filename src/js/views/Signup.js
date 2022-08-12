cd02972b4458e0d3617f64417f25eea7f61a0b3e
3rd-Web <web_3@icloud.com>
2022-08-11 23:08:40 -0400

    Minor changes to Homepage

---
+++ src/js/views/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Signup = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>SIGN UP PAGE</h1>
+      <a href="/create-your-profile">
+        <button>Sign Up (redirect to quiz)</button>
+      </a>
+    </div>
+  );
+};
+
+export default Signup;
