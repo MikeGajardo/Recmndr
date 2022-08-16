b13a49837add782a7009d270061917d57d84dc3e
MikeGajardo <mikegajardo@gmail.com>
2022-08-16 00:12:40 +0000

    Updated Styles

--- src/js/component/Quiz.js
+++ src/js/component/Quiz.js
@@ -6,10 +6,23 @@ import "../../styles/quiz.css";
 import electronic from "../../img/quizpage/electronic.jpg";
 
 export const Quiz = () => {
-  const { gameState, setGameState, scoreCo, setScoreCo, scoreSo, setScoreSo, scoreUn, setScoreUn, scoreMe, setScoreMe, scoreIn, setScoreIn } = useContext(QuizContext);
+  const {
+    gameState,
+    setGameState,
+    scoreCo,
+    setScoreCo,
+    scoreSo,
+    setScoreSo,
+    scoreUn,
+    setScoreUn,
+    scoreMe,
+    setScoreMe,
+    scoreIn,
+    setScoreIn,
+  } = useContext(QuizContext);
   const [currQuestion, setCurrQuestion] = useState(0);
-  const [optionChosen, setOptionChosen] = useState('');
-  const history = useHistory()
+  const [optionChosen, setOptionChosen] = useState("");
+  const history = useHistory();
 
   const nextQuestion = () => {
     if (Questions[currQuestion].contemporary == optionChosen) {
@@ -30,24 +43,48 @@ export const Quiz = () => {
     setCurrQuestion(currQuestion + 1);
   };
 
-    const finishQuiz = () => {
-    if (scoreCo > scoreSo && scoreCo > scoreUn && scoreCo > scoreMe && scoreCo > scoreIn) {
-      history.push('/my-profile/contemporary') 
+  const finishQuiz = () => {
+    if (
+      scoreCo > scoreSo &&
+      scoreCo > scoreUn &&
+      scoreCo > scoreMe &&
+      scoreCo > scoreIn
+    ) {
+      history.push("/my-profile/contemporary");
     }
-    if (scoreSo > scoreCo && scoreSo > scoreUn && scoreSo > scoreMe && scoreSo > scoreIn) {
-      history.push('/my-profile/sophisticated') 
+    if (
+      scoreSo > scoreCo &&
+      scoreSo > scoreUn &&
+      scoreSo > scoreMe &&
+      scoreSo > scoreIn
+    ) {
+      history.push("/my-profile/sophisticated");
     }
-    if (scoreUn > scoreSo && scoreUn > scoreCo && scoreUn > scoreMe && scoreUn > scoreIn) {
-      history.push('/my-profile/unpretentious') 
+    if (
+      scoreUn > scoreSo &&
+      scoreUn > scoreCo &&
+      scoreUn > scoreMe &&
+      scoreUn > scoreIn
+    ) {
+      history.push("/my-profile/unpretentious");
     }
-    if (scoreMe > scoreSo && scoreMe > scoreCo && scoreMe > scoreUn && scoreMe > scoreIn) {
-      history.push('/my-profile/mellow') 
+    if (
+      scoreMe > scoreSo &&
+      scoreMe > scoreCo &&
+      scoreMe > scoreUn &&
+      scoreMe > scoreIn
+    ) {
+      history.push("/my-profile/mellow");
     }
-    if (scoreIn > scoreSo && scoreIn > scoreCo && scoreIn > scoreUn && scoreIn > scoreMe) {
-      history.push('/my-profile/intense') 
-    } 
+    if (
+      scoreIn > scoreSo &&
+      scoreIn > scoreCo &&
+      scoreIn > scoreUn &&
+      scoreIn > scoreMe
+    ) {
+      history.push("/my-profile/intense");
+    }
   };
-
 
   return (
     <div className="container mt-4">
@@ -59,38 +96,62 @@ export const Quiz = () => {
           <div className="container text-center">
             <div className="question">
               <p>&nbsp;</p>
-              <h1>{Questions[currQuestion].prompt}</h1>
+              <h3>{Questions[currQuestion].prompt}</h3>
             </div>
-            <div className="d-flex justify-content-center answer">
-              <button className="answerbtn" onClick={() => setOptionChosen(Questions[currQuestion].contemporary)}>
+            <div className="d-flex justify-content-center answer2">
+              <button
+                className="btn btn-light answerbtn"
+                onClick={() =>
+                  setOptionChosen(Questions[currQuestion].contemporary)
+                }
+              >
                 {Questions[currQuestion].contemporary}
               </button>
             </div>
             <div className="d-flex justify-content-center answer2">
-              <button className="answerbtn" onClick={() => setOptionChosen(Questions[currQuestion].sophisticated)}>
+              <button
+                className="btn btn-light answerbtn"
+                onClick={() =>
+                  setOptionChosen(Questions[currQuestion].sophisticated)
+                }
+              >
                 {Questions[currQuestion].sophisticated}
               </button>
             </div>
             <div className="d-flex justify-content-center answer2">
-              <button className="answerbtn" onClick={() => setOptionChosen(Questions[currQuestion].unpretentious)}>
+              <button
+                className="btn btn-light answerbtn"
+                onClick={() =>
+                  setOptionChosen(Questions[currQuestion].unpretentious)
+                }
+              >
                 {Questions[currQuestion].unpretentious}
               </button>
             </div>
             <div className="d-flex justify-content-center answer2">
-              <button className="answerbtn" onClick={() => setOptionChosen(Questions[currQuestion].mellow)}>
+              <button
+                className="btn btn-light answerbtn"
+                onClick={() => setOptionChosen(Questions[currQuestion].mellow)}
+              >
                 {Questions[currQuestion].mellow}
               </button>
             </div>
             <div className="d-flex justify-content-center answer2">
-              <button className="answerbtn" onClick={() => setOptionChosen(Questions[currQuestion].intense)}>
+              <button
+                className="btn btn-light answerbtn"
+                onClick={() => setOptionChosen(Questions[currQuestion].intense)}
+              >
                 {Questions[currQuestion].intense}
               </button>
             </div>
-
             {currQuestion == Questions.length - 1 ? (
-              <button onClick={finishQuiz}>Finish Quiz</button>
+              <button className="btn btn-light ansbtnstart" id="complete" onClick={finishQuiz}>
+                Finish Quiz
+              </button>
             ) : (
-              <button onClick={nextQuestion}>Next Question</button>
+              <button className="btn btn-light ansbtnstart" id="complete" onClick={nextQuestion}>
+                Next Question
+              </button>
             )}
           </div>
         </div>
