c95f4c9d4c52a35f0ee18bcd8579c1f7f5d24714
Luffy85 <macias85ale@gmail.com>
2022-08-17 23:44:30 +0000

    casi todo

--- src/js/views/Login.js
+++ src/js/views/Login.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { UserAuth } from "../../Context/AuthContext";
 import { Card, Alert, Form, Button, Container } from "react-bootstrap";
-
-
+import turntables from "../../img/homepage/turntables.jpg"
 
 export const Login = () => {
   const [email, setEmail] = useState("");
@@ -11,7 +10,6 @@ export const Login = () => {
   const [error, setError] = useState("");
   const { signIn } = UserAuth();
   const history = useHistory();
- 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,67 +22,83 @@ export const Login = () => {
       console.log(e.message);
     }
 
-//     const dbRef = ref(getDatabase());
-// get(child(dbRef, `usuarios/{id}`)).then((snapshot) => {
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//   }
-// }).catch((error) => {
-//   console.error(error);
-// });
+    //     const dbRef = ref(getDatabase());
+    // get(child(dbRef, `usuarios/{id}`)).then((snapshot) => {
+    //   if (snapshot.exists()) {
+    //     console.log(snapshot.val());
+    //   } else {
+    //   }
+    // }).catch((error) => {
+    //   console.error(error);
+    // });
   };
 
   return (
     <>
-    <Container>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log in</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <div className="form-signin w-100 m-auto">
-            <div>
-              <h1 className="h3 mb-3 fw-normal">Sign in to you account</h1>
-              <p className="mt-5 mb-3 text-muted">
-                Don't have an account yet?{" "}
-                <Link to="/signup" className="underline">
-                  Sign up
-                </Link>
-              </p>
-            </div>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group
-                onChange={(e) => setEmail(e.target.value)}
-                className="mb-3"
-                controlId="formBasicEmail"
-              >
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+      <div className="w-full h-screen">
+        <Card className="position-relative">
+          <img
+            clasName="img-fluid img-thumbnail"
+            src={turntables}
+          />
+          <Card.Body
+            border="white"
+            style={{ width: "18rem" }}
+            className="bg-dark opacity-75 border border-5 position-absolute top-50 start-50 translate-middle"
+          >
+            <h2 className="text-center mb-4">Login</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <div className="form-signin w-100 m-auto">
+              <div>
+                <h1 className="h3 mb-3 text-white-50">Account</h1>
+                <p className="mt-5 mb-3 text-white-50">
+                  If you dont have a account{" "}
+                  <Link to="/Signup" className="underline">
+                    SignUp
+                  </Link>
+                </p>
+              </div>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mb-3"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label></Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
 
-              <Form.Group
-                onChange={(e) => setPassword(e.target.value)}
-                className="mb-3"
-                controlId="formBasicPassword"
-              >
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit" href="/create-your-profile">
-                Submit
-              </Button>
-            </Form>
-          </div>
-        </Card.Body>
-      </Card>
-      </Container>
+                <Form.Group
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mb-3"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label></Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check
+                    className="text-white-50"
+                    type="checkbox"
+                    label="Remember me"
+                  />
+                </Form.Group>
+                <Button variant="outline-primary" type="submit">
+                  Login
+                </Button>
+                <div className=" have-account w-100 text-center mt-2">
+                  Don't have an account?{" "}
+                  <Link className="signup" to="/Signup">
+                    SignUp
+                  </Link>
+                </div>
+              </Form>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 };
-export default Login
+export default Login;
--- src/js/views/Signup.js
+++ src/js/views/Signup.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { UserAuth } from "../../Context/AuthContext";
 import { Card, Alert, Form, Button, Container } from "react-bootstrap";
 import { getDatabase, ref, get, child } from "firebase/database";
+import signuptoday from "../../img/homepage/signuptoday.png";
 
 
 export const Singup = () => {
@@ -40,53 +41,67 @@ export const Singup = () => {
 
   return (
     <>
-    <Container>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <div className="form-signin w-100 m-auto">
-            <div>
-              <h1 className="h3 mb-3 fw-normal">Create an account</h1>
-              <p className="mt-5 mb-3 text-muted">
-               If you have a account{" "}
-                <Link to="/Login" className="underline">
-                  Login
-                </Link>
-              </p>
-            </div>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group
-                onChange={(e) => setEmail(e.target.value)}
-                className="mb-3"
-                controlId="formBasicEmail"
-              >
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+      <div className="w-full h-screen">
+        <Card className="position-relative">
+          <img
+            clasName="img-fluid img-thumbnail"
+            src={signuptoday}
+          />
+          <Card.Body
+            border="white"
+            style={{ width: "18rem" }}
+            className="bg-dark opacity-75 border border-5 position-absolute top-50 start-50 translate-middle"
+          >
+            <h2 className="text-center mb-4">Sign Up</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <div className="form-signin w-100 m-auto">
+              <div>
+                <h1 className="h3 mb-3 text-white-50">Create an account</h1>
+                <p className="mt-5 mb-3 text-white-50">
+                  If you have a account{" "}
+                  <Link to="/Login" className="underline">
+                    Login
+                  </Link>
+                </p>
+              </div>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mb-3"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label></Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-white-50">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Group
-                onChange={(e) => setPassword(e.target.value)}
-                className="mb-3"
-                controlId="formBasicPassword"
-              >
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit" href="/create-your-profile">
-                Submit
-              </Button>
-            </Form>
-          </div>
-        </Card.Body>
-      </Card>
-      </Container>
+                <Form.Group
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mb-3"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label></Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check className="text-white-50" type="checkbox" label="Remember me" />
+                </Form.Group>
+                <Button variant="outline-primary" type="submit">
+                 SignUp
+                </Button>
+                <div className=" have-account w-100 text-center mt-2">
+                  Already have an account?{" "}
+                  <Link className="signup" to="/login">
+                    Log In
+                  </Link>
+                </div>
+              </Form>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 };
