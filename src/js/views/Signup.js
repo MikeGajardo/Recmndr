61e0de66e233f975c6a43c5a525266a5f2f7e371
3rd-Web <web_3@icloud.com>
2022-08-24 18:33:52 +0200

    Enhanced sign in and log in views

--- src/js/views/Signup.js
+++ src/js/views/Signup.js
@@ -3,8 +3,11 @@ import { Link, useHistory } from "react-router-dom";
 import { UserAuth } from "../../Context/AuthContext";
 import { Card, Alert, Form, Button, Container } from "react-bootstrap";
 import { getDatabase, ref, get, child } from "firebase/database";
-import vinylhero from "../../img/homepage/vinylhero.png";
+import collage1 from "../../img/signup/collage1.jpg";
+import collage2 from "../../img/signup/collage2.jpg";
+import collage3 from "../../img/signup/collage3.jpg";
 
+import "../../styles/signup.css";
 
 export const Singup = () => {
   const [email, setEmail] = useState("");
@@ -23,8 +26,7 @@ export const Singup = () => {
       setError(error.message);
       console.log(error.message);
     }
-    
-   
+
     const dbRef = ref(getDatabase());
     get(child(dbRef, "/"))
       .then((snapshot) => {
@@ -41,68 +43,87 @@ export const Singup = () => {
 
   return (
     <>
-      <div className="w-full h-screen">
-        <Card className="position-relative">
-          <img
-            clasName="img-fluid img-thumbnail"
-            src={vinylhero}
-          />
-          <Card.Body
-            border="white"
-            style={{ width: "18rem" }}
-            className="bg-dark opacity-75 border border-5 position-absolute top-50 start-50 translate-middle"
-          >
-            <h2 className="text-center mb-4">Sign Up</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <div className="form-signin w-100 m-auto">
-              <div>
-                <h1 className="h3 mb-3 text-white-50">Create an account</h1>
-                <p className="mt-5 mb-3 text-white-50">
-                  If you have a account{" "}
-                  <Link to="/Login" className="underline">
-                    Login
-                  </Link>
-                </p>
-              </div>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="mb-3"
-                  controlId="formBasicEmail"
-                >
-                  <Form.Label></Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-white-50">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-
-                <Form.Group
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="mb-3"
-                  controlId="formBasicPassword"
-                >
-                  <Form.Label></Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check className="text-white-50" type="checkbox" label="Remember me" />
-                </Form.Group>
-                <Button variant="outline-primary" type="submit">
-                 SignUp
-                </Button>
-                <div className=" have-account w-100 text-center mt-2">
-                  Already have an account?{" "}
-                  <Link className="signup" to="/login">
-                    Log In
-                  </Link>
-                </div>
-              </Form>
+      <div className="container-fluid d-flex mx-auto justify-content-center">
+        <div>
+          <img src={collage1} className="digga"></img>
+        </div>
+        <div>
+          <Card className="position-relative signcard">
+            <div className="container-fluid d-flex mx-auto justify-content-center">
+              <img src={collage2} className="digga"></img>
             </div>
-          </Card.Body>
-        </Card>
+            <Card.Body
+              border="white"
+              style={{ width: "21rem" }}
+              className="bg-dark border border-2 position-absolute top-50 start-50 translate-middle cardbod"
+            >
+              <h2 className="text-center mb-4 textswag">sign up</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <div className="form-signin w-100 m-auto">
+                <div>
+                  <h1 className="h3 mb-3 text-white-50 text-center basicbitch">
+                    Create an account
+                  </h1>
+                  <p className="mt-5 mb-1 text-white-50 basicbitch">
+                    Have an account?{" "}
+                    <Link to="/Login" className="underline basicbitch">
+                      Login
+                    </Link>
+                  </p>
+                </div>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="mb-1"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                      className="basicbitch"
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                    <Form.Text className="text-white-50 basicbitch">
+                      We'll never share your email with anyone.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="mb-3 basicbitch"
+                    controlId="formBasicPassword"
+                  >
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                      className="basicbitch"
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check
+                      className="text-white-50 basicbitch"
+                      type="checkbox"
+                      label="Remember me"
+                    />
+                  </Form.Group>
+                  <Button
+                    className="basicbitch"
+                    variant="outline-primary"
+                    type="submit"
+                  >
+                    SIGN UP
+                  </Button>
+                </Form>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <img src={collage3} className="digga"></img>
+        </div>
       </div>
     </>
   );
 };
-export default Singup
+export default Singup;
